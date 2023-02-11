4bcb561398008eebdc277e99bdb431d8dc0da093
abc0907 <125103588+abc0907@users.noreply.github.com>
2023-02-11 13:17:34 +0800

    Update Main.java

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World");
-
+        "hello world";
         System.out.println("hello world");
     }
 }
